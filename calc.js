b0123c9c0897b7b5433df1c673b947d8ba87c4a1
EszopiCoder <pharm.coder@gmail.com>
2024-11-24 23:33:54 -0600

    Update calc.js
    Load CSV URL to dropdown

--- calc.js
+++ calc.js
@@ -1,3 +1,27 @@
+// Load CSV URL to dropdown
+window.onload = function() {
+  loadData();
+}
+// Resources:
+// https://stackoverflow.com/questions/39048654/how-to-enable-directory-indexing-on-github-pages
+// https://docs.github.com/en/rest/repos/contents?apiVersion=2022-11-28
+async function loadData() {
+  // Fetch file locations from Github
+  const response = await fetch('https://api.github.com/repos/eszopicoder/medbcalc/contents/Data');
+  const data = await response.json();
+  // Sort data Z to A
+  data.sort(function(a, b) {
+    return compareStrings(a.name, b.name);
+  })
+  // Add to dropdown
+  for (let file of data) {
+    let pricingData = document.createElement('option');
+    pricingData.value = file.download_url;
+    pricingData.innerHTML = file.name;
+    document.getElementById('pricingData').append(pricingData);
+  }
+}
+
 // Source: https://www.30secondsofcode.org/js/s/convert-csv-to-array-object-or-json/
 // Parse 1 row of CSV and convert to array
 const deserializeRow = (row, delimiter = ',') => {
